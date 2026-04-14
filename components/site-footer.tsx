c6d695038e80ba8d933b02ae8976f962ee3e9cb6
caliphsafe <84346820+caliphsafe@users.noreply.github.com>
2026-04-14 11:52:04 -0400

    Create site-footer.tsx

---
+++ components/site-footer.tsx
@@ -0,0 +1,9 @@
+export function SiteFooter() {
+  return (
+    <footer className="border-t border-black/5 px-6 py-10">
+      <div className="mx-auto max-w-7xl text-sm text-black/60">
+        ELÖRE Atelier
+      </div>
+    </footer>
+  );
+}
