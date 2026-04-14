7fbe1dc8042134d1bacfb3a28e37eb8ec3e442fe
caliphsafe <84346820+caliphsafe@users.noreply.github.com>
2026-04-14 17:08:09 -0400

    Update site-shell.tsx

--- components/site-shell.tsx
+++ components/site-shell.tsx
@@ -5,16 +5,19 @@ import { SiteHeader } from "@/components/site-header";
 import { SiteFooter } from "@/components/site-footer";
 import { CartDrawer } from "@/components/cart/cart-drawer";
 import { CartProvider } from "@/components/cart/cart-provider";
+import { WishlistProvider } from "@/components/wishlist/wishlist-provider";
 
 export function SiteShell({ children }: { children: React.ReactNode }) {
   const [cartOpen, setCartOpen] = useState(false);
 
   return (
     <CartProvider>
-      <SiteHeader onOpenCart={() => setCartOpen(true)} />
-      <main>{children}</main>
-      <SiteFooter />
-      <CartDrawer open={cartOpen} onClose={() => setCartOpen(false)} />
+      <WishlistProvider>
+        <SiteHeader onOpenCart={() => setCartOpen(true)} />
+        <main>{children}</main>
+        <SiteFooter />
+        <CartDrawer open={cartOpen} onClose={() => setCartOpen(false)} />
+      </WishlistProvider>
     </CartProvider>
   );
 }
