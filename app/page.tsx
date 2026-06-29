5a822c128b8e21d6243302014bfa4c0d771f3e1e
caliphsafe <84346820+caliphsafe@users.noreply.github.com>
2026-06-29 11:16:17 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { Hero } from "@/components/hero";
 import { AtelierStory } from "@/components/home/atelier-story";
-import { CustomJourney } from "@/components/home/custom-journey";
+import { ExperiencesPreview } from "@/components/home/experiences-preview";
 import { FeaturedCollections } from "@/components/home/featured-collections";
 import { HomeCta } from "@/components/home/home-cta";
 import { LookbookPreview } from "@/components/home/lookbook-preview";
@@ -12,7 +12,7 @@ export default function HomePage() {
       <FeaturedCollections />
       <LookbookPreview />
       <AtelierStory />
-      <CustomJourney />
+      <ExperiencesPreview />
       <HomeCta />
     </>
   );
